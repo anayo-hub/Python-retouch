e9c3611e872f87d5788fd30239f3b4e230495ddb
criss <anayocrescent2@gmail.com>
2026-07-11 16:02:58 +0100

    variables

---
+++ strings.py
@@ -0,0 +1,10 @@
+# name = 'crescent'
+# age = 21
+
+# print('hello my name is {name} and i am {age} years old.'.format(name=name, age=age))
+# print(f"name - {name}: age - {age}")
+
+# string methods
+s = "hello world"
+
+print(s.replace('hello', 'criss'))
