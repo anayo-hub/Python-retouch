af0b333c2c4de6ce34cf1bb3abdff30908088f99
criss <anayocrescent2@gmail.com>
2026-07-12 22:52:24 +0100

    class initated

--- module.py
+++ module.py
@@ -1,21 +1,49 @@
 # import datetime
-from camelcase import CamelCase
+# from camelcase import CamelCase
 
 
 # today = datetime.date.today()
 
-c = CamelCase()
+# c = CamelCase()
 
 # print(c.hump('hello world'))
 
 # print(today)
 
 class User:
-    def __init__(self, name, age, town):
+    def __init__(self, name, age, email):
         self.name = name
         self.age = age
-        self.town = town
+        self.email = email
+        
+    def greeting(self):
+        return  f'welcome my name is {self.name}, i am {self.age} : email is  {self.email}'
+    
+class Customer(User):
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.balance = 0
+    
+    def set_balance(self, balance):
+        self.balance = balance 
+        
+    # def greeting(self):
+    #     return  f'welcome my name is {self.name} i am {self.age} : email is  {self.email} and my balance is {self.balance}'
 
-crescent = User( 'anayo crescent', 30, "opi")
+# init an user obj
+crescent = User( 'anayo crescent', 32, "anayocrescent@gmail.com")
 
-print(c.hump(crescent.name))
+# init a customer obj
+janet = Customer('Janet medea', 37, 'traversy@gmail.com')
+
+janet.set_balance(500)
+
+
+print(janet.greeting())
+
+
+print(crescent.greeting())
+
+
